2c7c6356477a5f0f1cb84642d20278c67d4bf464
Seth Walton <sdwalton83@gmail.com>
2017-12-16 01:16:22 -0600

    better still need screenshots

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,47 +17,80 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+  displayInventory();
 });
+// displayInventory Function
+function displayInventory() {
+
+	queryStr = 'SELECT * FROM Products';
+
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+		var strOut = '';
+		for (var i = 0; i < data.length; i++) {
+			strOut = '';
+			strOut += 'Item ID: ' + data[i].ID + '| ';
+			strOut += 'Product Name: ' + data[i].productName + '| ';
+			strOut += 'Department: ' + data[i].departmentName + '| ';
+			strOut += 'Price: $' + data[i].price + '| ';
+			strOut += 'Stock: ' + data[i].stockQuantity;
+
+			console.log(strOut);
+		}
+
+	  	console.log("---------------------------------------------------------------------\n");
+
+	  	start();
+	})
+}
+
 
 // function which prompts the user for what action they should take
 function start() {
 
 	connection.query("SELECT * FROM bamazon.Products", function(err, results) {
     if (err) throw err;
-//     // once you have the items, prompt the user for which they'd like to bid on
-console.log(results)
+
     inquirer
       .prompt([
 		{
 
           name: "choice",
-          type: "rawlist",
-          choices: function() {
-            var choiceArray = [];
-
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].productName);
-            }
-            return choiceArray;
-          },
-          message: "What is the ID number of the Product you would like to buy?"
+          type: "input",
+          message: "What is the ID number of the Product you would like to buy?",
+	         validate: function(value) {
+	          if (isNaN(value) === false) {
+	            return true;
+	          }
+	          return false;
+	        }
 		},
         {
           name: "amount",
           type: "input",
-          message: "How many units would you like to buy?"
+          message: "How many units would you like to buy?",
+			validate: function(value) {
+          	if (isNaN(value) === false) {
+            return true;
+          	}
+          	return false;
+        	}
         }
       ])
 	    .then(function(answer) {
-	    	var item = answer.choice;
+	    	var item = parseInt(answer.choice);
+
+	    	var itemNo = item - 1;
 	    	var quantitySold = parseInt(answer.amount);
-			var stockQuantity = results[0].stockQuantity;
-			var itemNo = results[0].ID;
-			var salePrice = results[0].price * answer.amount;
+			var stockQuantity = results[itemNo].stockQuantity;
+			var salePrice = results[itemNo].price * answer.amount;
+
+			// Call data from data base
 	    	var query = "SELECT * FROM Products WHERE ?";
-	    	connection.query(query, {productName:item}, function(err, data){
+	    	connection.query(query, {ID:item}, function(err, data){
 
 	    		if(err) throw err;
 	    		
@@ -66,8 +99,8 @@ console.log(results)
 				start();
 				} else if(stockQuantity > quantitySold){
 					stockQuantity = stockQuantity - quantitySold;
-
-					var updateQueryStr = 'UPDATE Products SET stockQuantity = ' + stockQuantity + ' WHERE ID = ' + itemNo;
+		// Write data to database
+					var updateQueryStr = 'UPDATE Products SET stockQuantity = ' + stockQuantity + ' WHERE ID = ' + item;
 					connection.query(updateQueryStr,  function(err, data) {
 						if (err) throw err;
 
@@ -77,12 +110,9 @@ console.log(results)
 
 						connection.end();
 					})
-					// console.log(data)
-					// console.log(answer)
-					// console.log(stockQuantity)
 
 				} else{
-					console.log("Insufficient Quantity");
+					console.log("Insufficient Quantity Please Select an amount less than " + stockQuantity);
 					start();
 				}
 	    	});
